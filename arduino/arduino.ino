dbaaf7a8a49737ea6bf1b82aa1c5eced1cac862c
Jacob Berger <jacob.berger@live.com>
2014-12-04 02:00:27 +0100

    provide 1 pot, 6 button inputs from arduino to serial output;

---
+++ arduino/arduino.ino
@@ -0,0 +1,37 @@
+int pot1 =0;
+int btn1 = 0;
+int btn2 = 0;
+int btn3 = 0;
+int btn4 = 0;
+int btn5 = 0;
+int btn6 = 0;
+
+void setup(){
+  
+  pinMode(2, INPUT);  
+  pinMode(3, INPUT); 
+  Serial.begin(9600);
+}
+
+void loop () {
+  pot1 = analogRead(A0);
+  btn1 = digitalRead(2);
+  btn2 = digitalRead(3);
+  btn3 = digitalRead(4);
+  btn4 = digitalRead(5);
+  btn5 = digitalRead(6);
+  btn6 = digitalRead(7);
+  
+  Serial.print(pot1);
+  Serial.println(
+    ","+String(btn1)+
+    ","+String(btn2)+
+    ","+String(btn3)+
+    ","+String(btn4)+
+    ","+String(btn5)+
+    ","+String(btn6)
+    );
+ 
+  delay(100);
+}
+
